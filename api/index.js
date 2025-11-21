20a4769943545332163273a1b203b3e16af9cc3b
vnsnclo <vnsnclo@gmail.com>
2025-11-21 19:19:54 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const Application = require('@waline/vercel');
 
-module。exports = Application({
+module.exports = Application({
   plugins: [],
   async postSave(comment) {
     // do what ever you want after comment saved
